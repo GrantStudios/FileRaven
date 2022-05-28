ffd2fabfc5e5fefdf3a6c3471177a368815712d5
GrantStudios <grant.studio.2021@gmail.com>
2022-05-27 21:54:57 -0400

    Added encryption strength options.

--- src/resources/index.js
+++ src/resources/index.js
@@ -2,6 +2,15 @@ const fileUploadArea = $('#file-upload-area')
 const fileUploadButton = $('#file-upload')
 const fileUploadLabel = $('#file-upload-label')
 
+const encryptionStrengthOptions = $('#encryption-strength-container .encryption-strength-option')
+const encryptionStrengthDescription = $('#encryption-strength-description')
+
+const encryptionStrengthDescriptions = {
+    "WEAK": "Quick encryption for less important documents",
+    "MEDIUM": "Recommended encryption for normal documents",
+    "HIGH": "Strong encryption for important documents"
+}
+
 fileUploadButton.on('click', function () {
     fileUploadArea.css('opacity', '0.6')
     window.fs.promptFile({
@@ -12,4 +21,11 @@ fileUploadButton.on('click', function () {
             fileUploadLabel.text(result[0])
         }
     })
+})
+
+encryptionStrengthOptions.on('click', function(e){
+    const s = $(e.currentTarget)
+    encryptionStrengthOptions.removeClass('selected-strength')
+    s.addClass('selected-strength')
+    encryptionStrengthDescription.show().text(encryptionStrengthDescriptions[s.find('h4').text().trim()])
 })
